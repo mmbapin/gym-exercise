a7344207783ae09e29cfd0a7dcdb9dab7a914e3b
mmbapin <bapinmm@gmail.com>
2022-06-26 22:16:40 +0600

    router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import { Box } from '@mui/material'
 import "./App.css"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import ExerciseDetails from "./pages/ExerciseDetails"
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/exercise/:id' element={<ExerciseDetails />}/>
       </Routes>
+      <Footer />
     </Box>
   )
 }
